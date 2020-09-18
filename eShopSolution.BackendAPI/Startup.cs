7bfcfbf0d322d660cd473e86060c1f1a0f3bccaf
luclt399471 <luc.le@dreamcloud.com.sg>
2020-09-18 16:22:30 +0700

    - Add Swagger

--- eShopSolution.BackendAPI/Startup.cs
+++ eShopSolution.BackendAPI/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace eShopSolution.BackendAPI
 {
@@ -33,6 +34,11 @@ namespace eShopSolution.BackendAPI
             //Declare API
             services.AddTransient<IPublicProductService, PublicProductService>();
             services.AddControllersWithViews();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger eShop", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +60,12 @@ namespace eShopSolution.BackendAPI
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger eShop V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
